eb7ba3e48cf78463c6e82c69549bdfaa3176a303
preveen_chandran <Preveen_Chandran@Dell.com>
2018-11-30 12:06:53 +0530

    QA Config changes

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,5 +1,5 @@
 module.exports = {
               //       imageServerPath:'http://localhost/RInteger/serviceproviders/',
- imageServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/',
-         imageUrl: process.env.IMAGE_URL_PATH || 'https://rinteger.com/assets/images/logohomepage.jpg'
+ imageServerPath: process.env.IMAGE_SERVER_PATH_QA || 'https://rinteger.com/qa/admin/images/',
+         imageUrl: process.env.IMAGE_URL_PATH_QA || 'https://rinteger.com/qa/assets/images/logohomepage.jpg'
 };   
